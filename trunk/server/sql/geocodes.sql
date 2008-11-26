f9128bf677b0ad413b21d9fc7f2c67ec55c72650
gabrielknight4 <gabrielknight4@702e31f1-8f52-0410-bde9-ad18456409ae>
2008-11-26 09:43:54 +0000

    Android Server: added geocodes for GoogleMaps desktop support

---
+++ trunk/server/sql/geocodes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE geocodes (
+          address varchar(255) NOT NULL default '',
+          lon float default NULL,
+          lat float default NULL,
+          PRIMARY KEY  (address)
+        );
+
