dd254150996adcf6d07e77da2d18992adca9c0f1
gabrielknight4 <gabrielknight4@702e31f1-8f52-0410-bde9-ad18456409ae>
2008-12-05 12:40:16 +0000

    Mobile version 2 compatible with sdk 1.0:Tolti gli errori presenti in LocationSelection.

--- mobile1.0/src/android/client/LocationSelection.java
+++ mobile1.0/src/android/client/LocationSelection.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.StringTokenizer;
 import org.xml.sax.SAXException;
-import com.google.android.maps.Point;
+import android.graphics.Point;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
@@ -24,16 +24,17 @@ import android.util.Xml;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
-import android.view.Menu.Item;
+import android.view.MenuItem;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
-import com.google.android.maps.OverlayController;
 
 /**
  * Questa activity mostra la nostra posizione sulla mappa, la posizione dei nostri amici e la posizione delle location inserite nel database Permette inoltre di modificare i parametri per le locations tramite un menu
@@ -58,14 +59,14 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 	private LinearLayout searchBar;
 	private EditText search;
 	private Button go;
-	private Point lastSearch;
+	private GeoPoint lastSearch;
 
 	/**
 	 * @uml.property  name="db"
 	 * @uml.associationEnd  
 	 */
 	private DBHelper db;
-	protected OverlayController myOverlayController = null; 
+	protected List<Overlay> overlays = null; 
 	private String lastAddress;
 	
 	@Override
@@ -84,9 +85,9 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 		select.setOnClickListener(this);
 		db = new DBHelper(this);
 
-		myOverlayController = myMap.createOverlayController();
+		overlays = this.myMap.getOverlays();
 		MyLocationOverlay myLocationOverlay = new MyLocationOverlay();
-		myOverlayController.add(myLocationOverlay, true); 
+		overlays.add(myLocationOverlay); 
 		myMap.invalidate();
 	}
 
@@ -108,17 +109,17 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		menu.add(0, SEARCH, "Search", R.drawable.search_icon);
-		menu.add(0, CHOOSE, "Choose here", R.drawable.here_icon);
-		menu.add(0, VIEW_LOCATIONS, "View locations", R.drawable.icon_list);
-		menu.add(0, CHOOSE_DEFAULT, "Set Default", R.drawable.here_default_icon);
+		menu.add(0, SEARCH, 0, "Search").setIcon(R.drawable.search_icon);
+		menu.add(0, CHOOSE, 0, "Choose here").setIcon(R.drawable.here_icon);
+		menu.add(0, VIEW_LOCATIONS, 0,"View locations").setIcon(R.drawable.icon_list);
+		menu.add(0, CHOOSE_DEFAULT, 0,"Set Default").setIcon(R.drawable.here_default_icon);
 
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(Item item) {
-		switch (item.getId()) {
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case SEARCH:
 			searchBar.setVisibility(View.VISIBLE);
 			break;
@@ -137,12 +138,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 	}
 	
 	private void selectCurrentPosition(){
-		Point p = myMap.getMapCenter();
+		GeoPoint p = myMap.getMapCenter();
 		new LocationDialog(this, p, false).show();
 	}
 
 	private void selectDefaultPosition(){
-		Point p = myMap.getMapCenter();
+		GeoPoint p = myMap.getMapCenter();
 		new LocationDialog(this, p, true).show();
 	}
 
@@ -151,12 +152,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			String xmlCode = YahooGeoAPI.getGeoCode(address);
 			YahooGeocodeHandler handler = new YahooGeocodeHandler();
 			Xml.parse(xmlCode, handler);
-			Point p = new Point((int)handler.getLatitudeAsLong(), (int)handler.getLongitudeAsLong());
+			GeoPoint p = new GeoPoint((int)handler.getLatitudeAsLong(), (int)handler.getLongitudeAsLong());
 			MapController mc = myMap.getController();
 			lastSearch = p;
 			lastAddress = address;
 			mc.animateTo(p);
-			mc.zoomTo(21);
+			mc.setZoom(21);
 		} catch (IOException e){
 
 		} catch (SAXException e) {
@@ -164,6 +165,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 		}
 	}
 
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 	/**
 	 * Classe per mostrare un dialog di selezione del metodo di contatto
 	 * 
@@ -174,7 +181,7 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 	class LocationDialog extends Dialog implements OnClickListener{
 
 
-		private Point p;
+		private GeoPoint p;
 		private Button setHome;
 		private Button setMobile;
 		private Button setWork;
@@ -191,13 +198,13 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 		 * @param defaultLocation true se stiamo inserendo la posizione di default false altrimenti. Se è a true il punto è ingorato
 		 * 
 		 */
-		public LocationDialog(Context context, Point p, boolean defaultLocation) {
+		public LocationDialog(Context context, GeoPoint p, boolean defaultLocation) {
 			super(context);
 			this.p=p;
 			this.defaultLocation = defaultLocation;
 			this.setTitle("Current position is:");
 			setContentView(R.layout.choose_dialog);
-			geoCoder = new Geocoder();
+			geoCoder = new Geocoder(context);
 			setMobile = (Button)findViewById(R.id.set_mobile);
 			setHome = (Button)findViewById(R.id.set_home);
 			setWork = (Button)findViewById(R.id.set_work);
@@ -243,10 +250,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			else {
 				String res = "";
 				try {
-					Address[] addr = geoCoder.getFromLocation(p.getLatitudeE6(), p.getLongitudeE6());
+					Double latitude = Double.parseDouble(String.valueOf(p.getLatitudeE6())) * 1E6;
+					Double longitude = Double.parseDouble(String.valueOf(p.getLongitudeE6())) * 1E6;
+					List<Address> addr = geoCoder.getFromLocation(latitude, longitude, 1);
 					if (addr != null){
-						if (addr.length > 0){
-							res = addr[0].toString();
+						if (!addr.isEmpty()){
+							res = addr.get(0).toString();
 						}
 					}
 				} catch (IOException e) {
@@ -270,10 +279,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			else {
 				String res = "";
 				try {
-					Address[] addr = geoCoder.getFromLocation(p.getLatitudeE6(), p.getLongitudeE6());
+					Double latitude = Double.parseDouble(String.valueOf(p.getLatitudeE6())) * 1E6;
+					Double longitude = Double.parseDouble(String.valueOf(p.getLongitudeE6())) * 1E6;
+					List<Address> addr = geoCoder.getFromLocation(latitude, longitude, 1);
 					if (addr != null){
-						if (addr.length > 0){
-							res = addr[0].toString();
+						if (!addr.isEmpty()){
+							res = addr.get(0).toString();
 						}
 					}
 				} catch (IOException e) {
@@ -296,10 +307,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			else {
 				String res = "";
 				try {
-					Address[] addr = geoCoder.getFromLocation(p.getLatitudeE6(), p.getLongitudeE6());
+					Double latitude = Double.parseDouble(String.valueOf(p.getLatitudeE6())) * 1E6;
+					Double longitude = Double.parseDouble(String.valueOf(p.getLongitudeE6())) * 1E6;
+					List<Address> addr = geoCoder.getFromLocation(latitude, longitude, 1);
 					if (addr != null){
-						if (addr.length > 0){
-							res = addr[0].toString();
+						if (!addr.isEmpty()){
+							res = addr.get(0).toString();
 						}
 					}
 				} catch (IOException e) {
@@ -322,10 +335,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			else {
 				String res = "";
 				try {
-					Address[] addr = geoCoder.getFromLocation(p.getLatitudeE6(), p.getLongitudeE6());
+					Double latitude = Double.parseDouble(String.valueOf(p.getLatitudeE6())) * 1E6;
+					Double longitude = Double.parseDouble(String.valueOf(p.getLongitudeE6())) * 1E6;
+					List<Address> addr = geoCoder.getFromLocation(latitude, longitude, 1);
 					if (addr != null){
-						if (addr.length > 0){
-							res = addr[0].toString();
+						if (!addr.isEmpty()){
+							res = addr.get(0).toString();
 						}
 					}
 				} catch (IOException e) {
@@ -348,10 +363,12 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			else {
 				String res = "";
 				try {
-					Address[] addr = geoCoder.getFromLocation(p.getLatitudeE6(), p.getLongitudeE6());
+					Double latitude = Double.parseDouble(String.valueOf(p.getLatitudeE6())) * 1E6;
+					Double longitude = Double.parseDouble(String.valueOf(p.getLongitudeE6())) * 1E6;
+					List<Address> addr = geoCoder.getFromLocation(latitude, longitude, 1);
 					if (addr != null){
-						if (addr.length > 0){
-							res = addr[0].toString();
+						if (!addr.isEmpty()){
+							res = addr.get(0).toString();
 						}
 					}
 				} catch (IOException e) {
@@ -377,39 +394,36 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 	 */
 	protected class MyLocationOverlay extends Overlay {
 		private Canvas canvas;
-		private PixelCalculator calculator;
 		private Paint paint;
+		private MapView mV;
 		
 		@Override
-		public void draw(Canvas canvas, PixelCalculator calculator, boolean shadow) {
-			super.draw(canvas, calculator, shadow);
+		public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+			super.draw(canvas, mapView, shadow);
 			this.canvas = canvas;
-			this.calculator = calculator;
-
-
+			this.mV = mapView;
+			
 			paint = new Paint();
 			paint.setTextSize(14);
 
 			LocationManager myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-			Location l = myLocationManager.getCurrentLocation("gps");
-
-//			// Setup our "brush"/"pencil"/ whatever...
-//			Paint paint = new Paint();
-//			paint.setTextSize(14);
+			Location l = myLocationManager.getLastKnownLocation("gps");
 
 
 			// Create a Point that represents our GPS-Location
 			Double lat = l.getLatitude() * 1E6;
 			Double lng = l.getLongitude() * 1E6;
 
-			drawCircle(lat.intValue(), lng.intValue(), "ME", 156, 192, 36);
+			GeoPoint point = new GeoPoint(lat.intValue(), lng.intValue());
+			drawCircle(point , "ME", 156, 192, 36);
 			
           //Aggiungo le mie posizioni prendendole dal DBHelper
 			List<DBHelper.Location> ll = db.fetchAllRows();
 
 			for (android.client.DBHelper.Location loc : ll ){
-				drawCircle(loc.latitude, loc.longitude, loc.preferred, 230, 0, 0);
+				GeoPoint point_others = new GeoPoint(loc.latitude, loc.longitude);
+				drawCircle(point_others, loc.preferred, 230, 0, 0);
 
 			}
 			
@@ -418,46 +432,47 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 					android.provider.Contacts.ContactMethods.PERSON_ID
 			};
 			Cursor user = getContentResolver().query(Contacts.ContactMethods.CONTENT_URI, projection, "kind="+Settings.MY_CONTACTS_KIND, null, null);
-			while (user.next()){
+			while (user.moveToNext()){
 				int user_id = user.getInt(user.getColumnIndex(android.provider.Contacts.ContactMethods.PERSON_ID));
 
 				String[] projection2 = new String[] {
 						android.provider.Contacts.People.NAME
 				};
 				Cursor nome_c = getContentResolver().query(Contacts.People.CONTENT_URI, projection2, "people._id="+user_id, null, null);
-				if (nome_c.next()){
+				if (nome_c.moveToNext()){
 					String nome = nome_c.getString(nome_c.getColumnIndex(Contacts.People.NAME));
 
 					String[] projection3 = new String[] {
 							android.provider.Contacts.ContactMethods.DATA
 					};
-					Cursor pos_c = getContentResolver().query(Contacts.ContactMethods.CONTENT_URI, projection3, Contacts.ContactMethods.PERSON_ID+"="+user_id+" AND kind="+Contacts.ContactMethods.POSTAL_KIND, null, null);
-					pos_c.next();
+					Cursor pos_c = getContentResolver().query(Contacts.ContactMethods.CONTENT_URI, projection3, Contacts.ContactMethods.PERSON_ID+"="+user_id+" AND kind="+Contacts.ContactMethods.CONTENT_POSTAL_TYPE, null, null);
+					pos_c.moveToNext();
 					String pos = pos_c.getString(pos_c.getColumnIndex(Contacts.ContactMethods.DATA));
 
 					StringTokenizer tok = new StringTokenizer(pos.substring(1), ",");
 					Double latitude = Double.parseDouble(tok.nextToken()) * 1E6;
 					Double longitude = Double.parseDouble(tok.nextToken()) * 1E6;
-					drawCircle( latitude.intValue() , longitude.intValue(), nome, 0, 0, 200);
+					GeoPoint point_last = new GeoPoint(latitude.intValue() , longitude.intValue());
+					drawCircle(point_last, nome, 0, 0, 200);
 				}
 			}
+			return true;
 
 		}
 
 
-		private void drawCircle(int latitude, int longitde, String text, int R, int G, int B ){
+		private void drawCircle(GeoPoint mypoint, String text, int R, int G, int B ){
 
 
 			// Create a Point that represents our GPS-Location
-			Point point = new Point(latitude, longitde);
+			Point myScreenCoords = new Point();
 
-			int[] myScreenCoords = new int[2];
 			// Converts lat/lng-Point to OUR coordinates on the screen.
-			calculator.getPointXY(point, myScreenCoords);
+			mV.getProjection().toPixels(mypoint, myScreenCoords);
 
 			// Draw a circle for our location
-			RectF oval = new RectF(myScreenCoords[0] - 5, myScreenCoords[1] + 5,
-					myScreenCoords[0] + 5, myScreenCoords[1] - 5);
+			RectF oval = new RectF(myScreenCoords.x - 5, myScreenCoords.y + 5,
+					myScreenCoords.x + 5, myScreenCoords.y - 5);
 
 			// Setup a color for our location
 			paint.setStyle(Style.FILL);
@@ -466,7 +481,7 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 
 			// Draw our name
 			canvas.drawText(text,
-					myScreenCoords[0] +9, myScreenCoords[1], paint);
+					myScreenCoords.x +9, myScreenCoords.y, paint);
 
 			// Change the paint to a 'Lookthrough' Android-Green
 //			paint.setARGB(80, 156, 192, 36);
@@ -479,15 +494,10 @@ public class LocationSelection extends MapActivity implements OnClickListener{
 			// With a black stroke around the oval we drew before.
 			paint.setARGB(255,0,0,0);
 			paint.setStyle(Style.STROKE);
-			canvas.drawCircle(myScreenCoords[0], myScreenCoords[1], 5, paint);
+			canvas.drawCircle(myScreenCoords.x, myScreenCoords.y, 5, paint);
 
 
 		}
 	}
 
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
 }
